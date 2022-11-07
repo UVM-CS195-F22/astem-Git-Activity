773a76142a043f118d18bde8171f1f8174f87e32
anthonystem <anthony_stem@outlook.com>
2022-11-07 16:31:50 -0500

    Implemented establish_connection().

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sqlite3
 import sys
 
+def establish_connection(database_name):
+    try:
+        connection = sqlite3.connect(database_name)
+        cursor = connection.cursor()
+    except:
+        print(f"Error trying to connect to database '{database_name}'.")
+    
+    return connection, cursor
+    
+
 def main():
     if len(sys.argv) == 4:
         db = sys.argv[1]
         table = sys.argv[2]
         column = sys.argv[3]
+        
+        establish_connection(db)
         
         print(f"{db}, {table}, {column}")
     else:
